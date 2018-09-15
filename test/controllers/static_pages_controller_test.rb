d7f2886fbb22f15505c4246d5a80b340833f5fdd
Muhammd Rizwan <rizu3661@gmail.com>
2018-09-15 15:40:48 +0500

    chapter 3 sample app

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,22 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title","Home"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title","help"
   end
-
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title","about"
+  end
+  test "should get contactus" do
+    get static_pages_contactus_url
+    assert_response :success
+    assert_select "title","contactus"
+  end
 end
