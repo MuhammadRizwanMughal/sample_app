9bddd2a5fd1f54ba3b226ab11773d9b89feb6119
Muhammd Rizwan <rizu3661@gmail.com>
2018-09-18 12:34:50 +0500

    All tests are passed

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,24 +1,29 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+  test "should get root" do
+    get '/'
     assert_response :success
-    assert_select "title", "Home"
+  end
+
+  test "should get home" do
+    get '/home'
+    assert_response :success
+    assert_select "title", "Home | Ruby on rails tatorials"
   end
   test "should get help" do
-    get static_pages_help_url
+    get '/help'
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "help | Ruby on rails tatorials"
   end
   test "should get about" do
-    get static_pages_about_url
+    get '/about'
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | Ruby on rails tatorials"
   end
   test "should get contactus" do
-    get static_pages_contactus_url
+    get '/contactus'
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Contact us | Ruby on rails tatorials"
   end
 end
