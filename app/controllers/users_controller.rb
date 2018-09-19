427bb0dc2673a68687c796bce5b141e3a49dc8e0
Muhammd Rizwan <rizu3661@gmail.com>
2018-09-19 20:43:38 +0500

    sign up page created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
+  def show
+  @user = User.find(params[:id])
+  end
   def new
+    @user = User.new
+  end
+  def create
+    @user=User.new(user_params)
+    if @user.save
+      redirect_to home_path
+      flash[:success] = "Welcome to the sample Application"
+      #here will b code
+
+    else
+      render 'new'
+    end
+  end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmations)
+  end
+  def index
+
   end
 end
